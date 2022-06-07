cb82a7efdcf505a10a0f5b2d47716ed2a4ebbfe1
lautaro2021 <lautaroarnay@gmail.com>
2022-06-06 22:34:20 -0300

    Finished Proyect

--- client/src/components/CardCreate.jsx
+++ client/src/components/CardCreate.jsx
@@ -20,20 +20,16 @@ function validate(input) {
   //   errors.temperaments = "Please, select at least one temperament";
   else if (!input.heightMax || input.heightMax < 1 || input.heightMax > 100)
     errors.heightMax = "Please set correct Max Height";
-  else if (
-    !input.heightMin ||
-    input.heightMin < 1 ||
-    input.heightMin >= input.heightMax
-  )
+  else if ( !input.heightMin || input.heightMin < 1 || Number(input.heightMin) >= Number(input.heightMax))
     errors.heightMin = "Please set correct Min Height";
   else if (!input.weightMax || input.weightMax < 1 || input.weightMax > 100)
     errors.weightMax = "Please set correct Max Weight";
-  else if (
-    !input.weightMin ||
-    input.weightMin < 1 ||
-    input.weightMin >= input.weightMax
-  )
+  else if (!input.weightMin || input.weightMin < 1 || Number(input.weightMin) >= Number(input.weightMax))
     errors.weightMin = "Please set correct Min Weight";
+  else if (input.life_spanMax && (input.life_spanMax < 1 || input.life_spanMax > 100))
+    errors.life_spanMax = "Please set correct Max Life Span";
+  else if (input.life_spanMin && (input.life_spanMin < 1 || Number(input.life_spanMin) > Number(input.life_spanMax)))
+    errors.life_spanMin = "Please set correct Min Life Span";
   else if (!input.image || urlValidator.test(input.image) == false) errors.image = "Image cannot be null or incorrect";
 
   return errors;
@@ -279,8 +275,10 @@ function DogCreate() {
                     {errors.heightMin && <p>{errors.heightMin}</p>}
                     {errors.weightMax && <p>{errors.weightMax}</p>}
                     {errors.weightMin && <p>{errors.weightMin}</p>}
+                    {errors.life_spanMax && <p>{errors.life_spanMax}</p>}
+                    {errors.life_spanMin && <p>{errors.life_spanMin}</p>}
                     {errors.image && <p>{errors.image}</p>}
-                    {/* {errors.temperaments && <p>{errors.temperaments}</p>} */}
+                    
                   </div>
                 </div>
               )}
